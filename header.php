b22e4d21b53f6f4718fd86a61e2ce35d31ec23ae
Pete Schiebel <pete@voceconnect.com>
2011-06-30 08:45:11 -0400

    moving main stylesheet to root of theme folder bc that's where WP looks for it

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
   <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/img/favicon.ico"/>
   <link rel="apple-touch-icon" href="<?php bloginfo('template_directory');?>/img/voce_appletouch.png"/>
 
-  <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/style.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/style.css">
   <script src="<?php bloginfo('template_directory');?>/js/libs/modernizr-1.7.min.js"></script>
 
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
