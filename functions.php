ff92a1ec7c81599234f9afd48b1e5bda99f6f34a
Pete Schiebel <pete@voceconnect.com>
2011-10-05 14:16:45 -0400

    removing action to save 4 queries per @osh request

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
 remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
 remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
 remove_action( 'wp_head', 'wp_generator' );
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
 add_filter( 'show_admin_bar', '__return_false' );
 
 /**
