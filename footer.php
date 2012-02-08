0e8a41b3633d717f1f3756ad65631549909d7259
Pete Schiebel <pete@voceconnect.com>
2012-02-08 11:01:02 -0500

    updating theme with sass and boilerplate 3.0 items

--- footer.php
+++ footer.php
@@ -10,12 +10,9 @@
 <script src="<?php get_template_directory_uri(); ?>/js/script.js"></script>
 <!-- end scripts-->
 
-<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
+<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
      chromium.org/developers/how-tos/chrome-frame-getting-started -->
-<!--[if lt IE 7 ]>
-  <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-  <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-<![endif]-->
+<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,24 +1,24 @@
 <!doctype html>
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html lang="en-us" class="no-js"> <!--<![endif]-->
-      <head>
-        <meta charset="utf-8">
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
 
-        <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-        <meta name="description" content="">
-        <meta name="author" content="">
+		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+		<meta name="description" content="<?php bloginfo('description');?>">
+		<meta name="author" content="">
 
-        <!-- Mobile viewport optimized: j.mp/bplateviewport -->
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico"/>
-        <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri() ?>/img/voce_appletouch.png"/>
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+		<!-- Mobile viewport optimized: j.mp/bplateviewport -->
+		<meta name="viewport" content="width=device-width">
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico"/>
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri() ?>/img/voce_appletouch.png"/>
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/print.css" media="print"/>
         <!--[if (gte IE 6)&(lte IE 8)]>
 			<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/libs/selectivizr-min.js"></script>
-			<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/libs/modernizr-2.0.6.min.js"></script>
+			<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/libs/modernizr-2.5.1-min.js"></script>
         <![endif]-->			
 
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
