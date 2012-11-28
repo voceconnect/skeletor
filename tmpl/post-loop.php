5c98670dab05bce7362f3d8b329830c4a092d57a
Pete Schiebel <pete@voceconnect.com>
2012-11-27 21:40:48 -0500

    updating skeletor to have a better bootstrap core. breaking out each style section and javascript section so its easy to only include what we need

--- tmpl/post-loop.php
+++ tmpl/post-loop.php
@@ -1,4 +1,4 @@
-<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<div <?php post_class('loop-item'); ?> id="post-<?php the_ID(); ?>">
     <?php if( is_single() ) : ?>
         <h1><?php the_title(); ?></h1>
     <?php else : ?>
