b595743e6058ca4d95ebfe70d2a246cb7d29c16d
Pete Schiebel <pete@voceconnect.com>
2013-11-26 16:50:41 -0500

    starting child theme of skeletor that will provide more options for a starter theme

---
+++ attachment.php
@@ -0,0 +1,31 @@
+<?php get_header();?>
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12" role="main" id="primary">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'attachment' ); ?>
+					
+					<?php 
+					// Get the parent post ID
+					$parent_id = $post->post_parent;
+					// Get the parent post Title
+					$parent_title = get_the_title( $parent_id );
+					// Get the parent post permalink
+					$parent_permalink = get_permalink( $parent_id );
+					
+					if($parent_permalink)
+						echo '<p><a href="' . $parent_permalink . '">&larr; ' . $parent_title . '</a></p>';
+					?>
+				<?php endwhile; // end of the loop. ?>
+
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php get_footer();?>
--- content-single.php
+++ content-single.php
@@ -5,6 +5,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if( has_post_thumbnail() ) {
+		echo '<div class="attachment-container">';
+		the_post_thumbnail();
+		echo '</div>';
+	}?>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 
