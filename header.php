59613549211203f2703efdcb5bad3ac5743875f4
Pete Schiebel <pete@voceconnect.com>
2014-01-03 11:34:57 -0500

    adding search form to header, 2013 style

--- header.php
+++ header.php
@@ -19,6 +19,14 @@
 	<header class="navbar navbar-inverse" role="banner" id="masthead">
 		<div class="container">
 			<div class="navbar-header">
+				<form role="search" method="get" class="search-form" action="/">
+					<label>
+						<span class="screen-reader-text">Search for:</span>
+						<input type="search" class="search-field" placeholder="Search …" value="" name="s" title="Search for:">
+					</label>
+					<input type="submit" class="search-submit" value="Search">
+				</form>
+				
 				<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
