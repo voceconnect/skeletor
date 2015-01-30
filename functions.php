9dc27931e9ff5c845eee12c6a7b9ab495f5f8177
Kevin Langley <me@kevinlangleyjr.com>
2015-01-29 19:38:26 -0500

    fixing editor styles

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  *
  * @package skeletor
  */
-add_editor_style( 'editor-style.css' );
+add_editor_style( 'editor-styles.css' );
 
 /**
  * Set the content width based on the theme's design and stylesheet.
